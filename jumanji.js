7c223a17b9aef6e0660c93adf414ae2791d7cf6b
MAlexGG <alexandragalarzag@gmail.com>
2020-11-25 11:18:25 +0100

    versionAlex2

--- jumanji.js
+++ jumanji.js
@@ -8,6 +8,31 @@ let ballRadius = 10;
 let PalaHeight= 10;
 let PalaWidth= 75;
 let PalaX= (canvas.width-PalaWidth)/2;
+let rightPressed = false;
+let leftPressed = false;
+
+document.addEventListener("keydown", KeyDownHandler, false);
+document.addEventListener("keyup" , KeyUpHandler, false);
+
+function KeyDownHandler (e) {
+
+
+    if (e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if (e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function KeyUpHandler (e) {
+    if (e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if (e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
 
 
 function drawBall() {
@@ -28,8 +53,10 @@ function drawPala() {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
     drawBall();
     drawPala();
+   
 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius){
         dx = -dx;
@@ -38,11 +65,21 @@ function draw() {
         dy= -dy;
     }
 
+    if (rightPressed && PalaX < canvas.width-PalaWidth) {
+        PalaX += 7;
+    }
+
+    else if (leftPressed && PalaX > 0) {
+        PalaX -= 7;
+    }
+
+
     x += dx;
     y += dy; 
 
-
 }
+
+
 
 setInterval(draw, 10);
 
